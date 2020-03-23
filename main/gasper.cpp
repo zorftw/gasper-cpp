f4f0444ce64872573917e5839399eb948e40876c
Zor <windows8nb@outlook.com>
2020-03-23 12:32:21 +0100

    Add files via upload

--- main/gasper.cpp
+++ main/gasper.cpp
@@ -46,10 +46,12 @@ void gasper::c_gasper::run()
 		auto local = std::make_unique<c_player>(sdk::instance->get_player(minecraft_inst));
 		auto world = std::make_unique<c_world>(sdk::instance->get_world(minecraft_inst));
 
+		/// Returns true when the local player object and the world object aren't null
 		const auto is_sane = [&]() {
 			return (local->get_object() && world->get_object());
 		};
 
+		/// Get closest player to local player (distance wise)
 		const auto get_closest_player = [&]() {
 			double dist = (std::numeric_limits<double>::max)();
 			std::shared_ptr<c_player> target = nullptr;
@@ -71,12 +73,24 @@ void gasper::c_gasper::run()
 			return target;
 		};
 
+		/// Simple lambda function that returns true if time has been passed
+		const auto has_passed = [&](float ms) {
+			if (GetTickCount64() - time > ms)
+			{
+				time = GetTickCount64();
+				return true;
+			}
+
+			return false;
+		};
+
 		/// We're ingame	
 		if (is_sane())
 		{
 			auto entity = get_closest_player();
 
-			if (entity)
+			/// use 30ms, because the game runs on a 20 tick system, but we want to smooth it out, so run it a tad more
+			if (entity && has_passed(30) )
 			{
 				auto angles = sdk::util::get_angles(local->get_position(), entity->get_position());
 
